2a519c469d2c8d7f55e47f4b06c0d97245ebce91
artem <arzek2015@gmail.com>
2022-09-04 14:51:12 +0800

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { Telegraf } from 'telegraf';
+import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const configService = await app.get(ConfigService);
+  const bot = new Telegraf(configService.get('TELEGRAM_BOT_TOKEN'));
+
+  bot.on('text', (ctx) => {
+    // Explicit usage
+
+    // Using context shortcut
+    ctx.reply(`Hello ${ctx.state.role}`);
+  });
+
+  await bot.launch();
 }
 bootstrap();
